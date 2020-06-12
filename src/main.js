929caafe92c6415d045acdd5f1ac17d50dba3ad1
Herman Dardon <hdardon91@gmail.com>
2020-06-12 11:41:18 -0700

    test

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-//TODO: import axios from "axios";
 
 Vue.config.productionTip = false;
 
