44c11ae794a06bf3df6d9f41cb8d3bfeeaf92eaa
Herman Dardon <hdardon91@gmail.com>
2020-06-12 11:31:00 -0700

    updated nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,24 @@
 <template>
   <div>
-    <b-navbar type="dark">
+    <b-navbar type="dark" toggleable="lg">
       <b-navbar-brand to="/">The Stock Trader</b-navbar-brand>
 
-      <b-navbar-nav>
-        <b-nav-item to="/portfolio">Portfolio</b-nav-item>
-        <b-nav-item to="/market">Market</b-nav-item>
-      </b-navbar-nav>
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item class="mr-3" @click="endDay">End of Day</b-nav-item>
-        <b-nav-item-dropdown class="mr-3" text="Save & Load" right>
-          <b-dropdown-item @click="loadData">Load</b-dropdown-item>
-          <b-dropdown-item @click="saveData">Save</b-dropdown-item>
-        </b-nav-item-dropdown>
-        <b-nav-text>Funds: {{ funds }}</b-nav-text>
-      </b-navbar-nav>
+      <b-navbar-toggle target="navbar-toggle-collapse"></b-navbar-toggle>
+      <b-collapse id="navbar-toggle-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/portfolio">Portfolio</b-nav-item>
+          <b-nav-item to="/market">Market</b-nav-item>
+        </b-navbar-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item class="mr-3" @click="endDay">End of Day</b-nav-item>
+          <b-nav-item-dropdown class="mr-3" text="Save & Load" left>
+            <b-dropdown-item @click="loadData">Load</b-dropdown-item>
+            <b-dropdown-item @click="saveData">Save</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-text>Funds: {{ funds }}</b-nav-text>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
   </div>
 </template>
